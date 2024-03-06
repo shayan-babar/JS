3471ad3f978c89c1c20520879ab7a4030d9ceb59
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-03-07 00:49:03 +0500

    Add MySelf

---
+++ script.js
@@ -0,0 +1,29 @@
+//MySelf 
+
+let myName = "Shayan"
+
+let Age = "18"
+
+let Passion = "Programming"
+
+let = Hobby = "Coding & Cricket"
+
+console.log(`Name:`, myName);
+
+console.log("Age:", Age);
+
+console.log("Passion:", Passion);
+
+console.log(`Hobby:`, Hobby);
+
+//
+
+/*let MySelf = {
+    Name: "Shayan",
+    Age: "18",
+    Passion: "Programmaing",
+    Qualification: "Intermediate in CS",
+    Hobby: "Coding & Cricket",
+}*/
+
+//
