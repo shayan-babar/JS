e0b6a880a0d3da2b592134d67f90d3db4c970c53
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-03-16 02:17:18 +0500

    Add QnA of Chapter 2

---
+++ script.js
@@ -0,0 +1,40 @@
+//Chapter 2 
+
+//Question # 1:
+let username;
+//
+//Question # 2:
+let myName = "Shayan Babar";
+//
+//Question # 3:
+let message;
+message = "Hello world";
+alert(message);
+//
+//Question # 4:
+let myname = "Shayan Babar";
+let Age = 18;
+let Qualification = "Intermediate in CS";
+let Hobbies = "Cricket and Coding";
+
+//display in the alert box
+let Mybiodata = "(My Bio Data)";
+Mybiodata += "Name: " + myname + "\n";
+Mybiodata += "Age: " + Age + "\n";
+Mybiodata += "Qualification: " + Qualification + "\n";
+Mybiodata += "Hobbies: " + Hobbies + "\n";
+alert(Mybiodata);
+//
+
+//Question # 6:
+let email = "shayanbabar128@gmail.com";
+let emailaddress = "My email address is: '" + email + "'";
+alert(emailaddress);
+//
+
+//Question # 7:
+let book = "A smarter way to learn JavaScript";
+
+//display in the alert box
+alert("I am currently reading the book: '" + book + "'");
+//
