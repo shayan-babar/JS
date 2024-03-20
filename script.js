002f7db6427697b230f96cc501660a4e12bc152f
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-03-21 01:39:30 +0500

    Add Question # 3 of chapter 2

--- script.js
+++ script.js
@@ -78,3 +78,11 @@ let "shayan"
 let @shayan
 let (shayan)*/
 //
+
+//Question # 3
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p></p>Variable names can only contain ,numbers,$ and _ . For example : $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter,$ or_ . For example : $name, _name or name</p>");
+document.write("<p></p>Variable names are case-sensitive</p>");
+document.write("<p></p>Variable names should not be JavaScript keywords</p>");
+//
