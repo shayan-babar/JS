50ab211a415c7135bd3ddda581f4ac05fc4f1722
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-03-28 04:31:08 +0500

    Add Q&N of chapter 6

--- script.js
+++ script.js
@@ -139,3 +139,36 @@ for (let i=1 ; i<=10 ; i++) {
 let temperature = "NNC";
 document.write("25C&#176 is 77F&#176; <br/> 70&#176;F is 21.11111111111111C&#176",'<br></br>');
 //
+
+//Chapter 6
+
+// Question # 1:
+let number = 10;
+document.write("The result value of a is: " + number + "<br/><br/>");
+
+++number;
+document.write("The value of ++a is: " + number + "<br/>");
+document.write("Now the value of a is: " + number + "<br/><br/>");
+
+number++;
+document.write("The value of a++ is: 11 <br/>");
+document.write("Now the value of a is: " + number + "<br/><br/>");
+
+--number;
+document.write("The value of --a is: " + number + "<br/>");
+document.write("Now the value of a is: " + number + "<br/><br/>");
+
+number--;
+document.write("The value of a-- is: 11 <br/>");
+document.write("Now the value of a is: " + number + "<br/><br/>");
+//
+
+// Question # 2:
+let a = 2;
+let b = 1;
+--a;
+--a - --b;
+--a - --b + ++b;
+document.write("a is = " + a + "<br/>");
+document.write("b is = " + b + "<br/>");
+//
