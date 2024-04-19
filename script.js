d4a966dc859b652802c8161c21956dbfd223ec72
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-04-20 02:39:46 +0500

    Input in matrix array  & Output in square values

--- script.js
+++ script.js
@@ -327,3 +327,30 @@ for (let i = 0; i < myArr.length; i++) {
 }
 
 //
+
+//Input matrix aaray 
+//Output in square values
+
+let matrix1 = [
+    [5, 2, 3],
+    [7, 2, 3],
+    [1, 0, 5],
+];
+let matrix2 = [
+    [5, 2, 3],
+    [7, 6, 3],
+    [7, 9, 8],
+];
+
+// result = [
+//   [10, 4, 6],
+//   [14, 8, 6],
+//   [8, 9, 13],
+// ];
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        console.log(matrix1[i][j] * matrix2[i][j]);
+    }
+}
+//
