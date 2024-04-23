7e00966640d3450df74731fcaf146ab8ea1cc104
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-04-24 02:48:20 +0500

    array and loop practice

--- script.js
+++ script.js
@@ -379,3 +379,24 @@ for (let i = 0; i < 3; i++) {
 }
 console.log(result);
 //
+
+//
+
+let matrix = [
+    [7, 3, 6],
+    [2, 4, 9],
+    [3, 7, 5],
+]
+
+let matrix2 = [
+    [3, 7, 5],
+    [2, 4, 9],
+    [7, 3, 6],
+]
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        console.log(matrix[i][j] + matrix2[i][j]);
+    }
+}
+//
