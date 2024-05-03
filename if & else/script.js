15bd02d687e434cefd3d40f25122d93ec022a2f0
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-04 01:12:06 +0500

    Update script.js

---
+++ if & else/script.js
@@ -0,0 +1,23 @@
+let weather = 40
+let score = 68
+//If Statement
+if (weather > 20) {
+    console.log('Weather very hot');
+}
+
+if (score > 50) {
+    console.log("Not Out");
+}
+
+if (50 > 20) {
+    console.log("50 greater then 20");
+}
+
+if (20 < 50) {
+    console.log("20 less then 50");
+}
+
+if (score < 72) {
+    console.log("Out");
+}
+//
