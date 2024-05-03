2757b74c3cc4c50674a456cd46d11a3442e686cd
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-04 01:15:58 +0500

    Update script.js

---
+++ array/script.js
@@ -0,0 +1,9 @@
+let Cities = ['Karachi', 'Lahore', 'Islamabad', 'Peshawar', 'Quetta', 'Faisalabad'];
+
+//Array methods
+console.log(Cities);
+Cities.push('Hyderabad')
+Cities.pop()
+Cities.unshift('Sialkot')
+Cities.shift()
+//
