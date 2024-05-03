26a04b979bb4ed3e9f5ee5df55f0a050024acbd1
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-04 01:13:47 +0500

    Update script.js

--- if & else/script.js
+++ if & else/script.js
@@ -21,3 +21,42 @@ if (score < 72) {
     console.log("Out");
 }
 //
+
+//
+let Pakistan = true;
+let India = false;
+let Palestine = true;
+let America = false;
+
+if (Palestine && Pakistan) {
+    console.log('We are stand with Palestine')
+} else {
+    console.log('We are not human')
+};
+
+if (India === America) {
+    console.log('Israel is mf');
+} else {
+    console.log('Israel is also mf😂');
+}
+//
+
+//
+let age = 18
+let since = 2005
+let month = 'August'
+
+console.log(age == '18');
+console.log(age === '18');
+console.log(age != 17);
+console.log(age != age);
+console.log(since + age - age);
+
+console.log(age++);
+if (age > 18) {
+    console.log('my age');
+} else {
+    console.log("i'm 19?");
+}
+//
+
