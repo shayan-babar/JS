c51d7501857bf85f76deb13097c4655e844c2c8d
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-09 01:25:14 +0500

    problem#2011.js

--- LeetCode/problem#2011.js
+++ LeetCode/problem#2011.js
@@ -1,1 +1,17 @@
-
+var finalValueAfterOperations = function (operations) {
+    var count = 0;
+    for (var i = 0; i < operations.length; i++) {
+        if (operations[i].includes('X++')) {
+            count = count + 1;
+        }
+        else if (operations[i].includes('++X')) {
+            count = count + 1;
+        } else if (operations[i].includes('X--')) {
+            count = count - 1;
+        }
+        else if (operations[i].includes('--X')) {
+            count = count - 1;
+        }
+    }
+    return count;
+};
