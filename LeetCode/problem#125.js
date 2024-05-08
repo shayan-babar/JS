4247ddfedc644b73fb96b7ab43ea04e7481d6436
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-09 01:26:33 +0500

    problem#125.js

--- LeetCode/problem#125.js
+++ LeetCode/problem#125.js
@@ -1,1 +1,12 @@
+var isPalindrome = function (str) {
+    let arr = str.split('')
+    let result = []
+    let alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
 
+    for (let i of arr) {
+        if (alphabet.includes(i.toLowerCase())) {
+            result.push(i.toLowerCase())
+        }
+    }
+    return (result.join('') == result.reverse().join(''))
+};
