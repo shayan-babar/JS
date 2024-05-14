0fc6968b1293d10937672d619d942737150e44dc
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-15 02:38:21 +0500

    fu

--- functions/script.js
+++ functions/script.js
@@ -7,9 +7,18 @@ function myFunction(p1, p2) {
 }
 //
 
+//
 function multiply(a, b) {
     return a * Number(b);
 }
 
 console.log(multiply("2", "3"));
+//
+
+//
+function average(a, b) {
+    return a + b / 2;
+}
+
+console.log(average(2, 1));
 //
