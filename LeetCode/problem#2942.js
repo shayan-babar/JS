b8268f77be030712ecd4d098e0f567fe7559af71
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-18 11:57:04 +0500

    problem#2942.js

--- LeetCode/problem#2942.js
+++ LeetCode/problem#2942.js
@@ -1,1 +1,14 @@
+const findWordsContaining = (words, x) => {
+    let result = [];
 
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length; j++) {
+            if (words[i][j] === x) {
+                result.push(i)
+                break;
+            }
+        }
+    }
+
+    return result;
+};
