096be85dfcc4cc093e9490bbaaf173aee815ca8f
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-05-18 12:00:40 +0500

    problem#258.js

--- LeetCode/problem#258.js
+++ LeetCode/problem#258.js
@@ -1,1 +1,13 @@
+const addDigits = (num) => {
+    let sum = num;
 
+    while (sum >= 10) {
+        let tempSum = 0;
+        let strSum = sum.toString();
+        for (let i = 0; i < strSum.length; i++) {
+            tempSum += parseInt(strSum[i]);
+        }
+        sum = tempSum;
+    }
+    return sum
+};
